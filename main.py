b71fec7dced0ced4121a24b74c88dd21808e8cc3
Ztac <melmeh42@gmail.com>
2022-06-09 12:37:35 +0300

    Basic Code Done

--- main.py
+++ main.py
@@ -18,28 +18,23 @@ for path in os.listdir(dir_path):
 def UserGiris():
     print("Klasor'u bosaltiyorum? Enter Yes or No")
     answer='wrong'
-    while answer not in ['YES','NO','Y','N','X']:
-        answer=input("Answer? (Yes or No for exit x):")
+    while answer not in ['YES','NO','Y','N']:
+        answer=input("Answer? (Yes or No):")
         answer=answer.upper()
-        if answer not in ['YES','NO','Y','N','X']:
+        if answer not in ['YES','NO','Y','N']:
             print("Sorry, invalid answer!")
     
     if answer in ['YES','Y']:
         return True
     elif answer in ['NO','N']:
         return False
-    else:
-        return answer
 
-
-dongu=True
-while dongu:    
-    answer=UserGiris()
-    if answer is True:
-        count=0
-        # LIST ITEM IN RES LIST AND CHECKS IF ITS IN ESSENTIALS IF NOT REMOVES ITEM
-        for iter in res:
-            if iter not in essentials:
-                os.remove(dir_path+'\\'+iter)
-                count+=1
-        print("{} amount of files removed".format(count))
+answer=UserGiris()
+if answer is True:
+    count=0
+    # LIST ITEM IN RES LIST AND CHECKS IF ITS IN ESSENTIALS IF NOT REMOVES ITEM
+    for iter in res:
+        if iter not in essentials:
+            os.remove(dir_path+'\\'+iter)
+            count+=1
+    print("{} amount of files removed".format(count))
